47d8e59f076badeef7e20dc5592a7e3af98d9c98
JJGantt <jaredgantt@gmail.com>
2025-12-02 18:29:26 -0500

    Initial Commit

---
+++ GolfTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GolfTracker
+//
+//  Created by Jared gantt on 12/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GolfTracker/GolfTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfTrackerApp.swift
+//  GolfTracker
+//
+//  Created by Jared gantt on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct GolfTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
